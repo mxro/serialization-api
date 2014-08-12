15bd75afc7f582cf4fd73f8160592312b5c1951c
mxro <mxro@nowhere.com>
2014-08-13 09:49:21 +1000

    Developing serialization API Autocommit Wed Aug 13 09:49:21 EST 2014

--- src/main/java/de/mxro/serialization/Serialization.java
+++ src/main/java/de/mxro/serialization/Serialization.java
@@ -1,5 +1,16 @@
 package de.mxro.serialization;
 
+import de.mxro.serialization.string.StringDestination;
+import de.mxro.serialization.string.StringSource;
+
 public class Serialization {
+
+	public static StringDestination createStringDestination() {
+		return new StringDestination();
+	}
+
+	public static StringSource createStringSource(String s) {
+		return new StringSource(s);
+	}
 
 }
--- src/main/java/de/mxro/serialization/jre/SerializationJre.java
+++ src/main/java/de/mxro/serialization/jre/SerializationJre.java
@@ -1,5 +1,17 @@
 package de.mxro.serialization.jre;
 
+import de.mxro.serialization.Serializer;
+import de.mxro.serialization.jre.internal.JavaSerializationProvider;
+
 public class SerializationJre {
+
+	/**
+	 * A serializer based on Java's built in Serialization mechanism.
+	 * 
+	 * @return
+	 */
+	public static Serializer<StreamSource, StreamDestination> newJavaSerializer() {
+	    return new JavaSerializationProvider();
+	}
 
 }
