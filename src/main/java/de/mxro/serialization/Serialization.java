5d38fded529de9839768f39c0704e6ae3bd090f7
mxro <mxro@nowhere.com>
2014-08-13 09:40:14 +1000

    a

---
+++ src/main/java/de/mxro/serialization/Serialization.java
@@ -0,0 +1,5 @@
+package de.mxro.serialization;
+
+public class Serialization {
+
+}
---
+++ src/main/java/de/mxro/serialization/internal/DummyEntryPoint.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright 2011 Max Erik Rohde http://www.mxro.de
+ * 
+ * All rights reserved.
+ ******************************************************************************/
+package de.mxro.serialization.internal;
+
+public class DummyEntryPoint {
+
+	public void onModuleLoad() {
+
+	}
+}
