6d527d023eba9fb053c0b362eed981456e2e2469
mxro <mxro@nowhere.com>
2014-09-18 14:03:39 +1000

    Reworking embedded server API Autocommit Thu Sep 18 14:03:39 EST 2014

--- src/main/java/de/mxro/serialization/Serializer.java
+++ src/main/java/de/mxro/serialization/Serializer.java
@@ -19,21 +19,24 @@ package de.mxro.serialization;
  */
 public interface Serializer<SourceType extends SerializationSource<?>, DestinationType extends SerializationDestination<?>> {
 
-	/**
-	 * Serializes a Java object into a {@link SerializationDestination}.
-	 * 
-	 * @param o The object to be serialized.
-	 * @param dest The destionation where to the object is to be serialized.
-	 * @return
-	 */
-	public boolean serialize(Object o, DestinationType dest);
+    /**
+     * Serializes a Java object into a {@link SerializationDestination}.
+     * 
+     * @param o
+     *            The object to be serialized.
+     * @param dest
+     *            The destionation where to the object is to be serialized.
+     * @return
+     */
+    public boolean serialize(Object o, DestinationType dest);
 
-	/**
-	 * Deserializes a Java object from a {@link SerializationSource}
-	 * 
-	 * @param source The source from which the object is to be deserialized.
-	 * @return The deserialized object.
-	 */
-	public Object deserialize(SourceType source);
+    /**
+     * Deserializes a Java object from a {@link SerializationSource}.
+     * 
+     * @param source
+     *            The source from which the object is to be deserialized.
+     * @return The deserialized object.
+     */
+    public Object deserialize(SourceType source);
 
 }
